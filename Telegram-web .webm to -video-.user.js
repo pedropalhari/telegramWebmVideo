0c574b87b39d809d21c853fe17772bb2d0442565
pedropalhari <pedropalhari@gmail.com>
2018-03-18 17:07:49 -0300

    Add files via upload

---
+++ Telegram-web .webm to -video-.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Telegram-web .webm to <video>
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://web.telegram.org/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setInterval(() => {
+  var textNodes = document.getElementsByClassName("im_message_text");
+  var webmTextNodes = [];
+
+  for (var textNode of textNodes)
+    if (
+      textNode.firstChild != null &&
+      textNode.firstChild.href != undefined &&
+      textNode.firstChild.href.indexOf(".webm") != -1
+    )
+      webmTextNodes.push(textNode.firstChild);
+
+  for (var videoLink of webmTextNodes) {
+    if (!videoLink.hasAlreadyBeenUpdated)
+      videoLink.insertAdjacentHTML(
+        "afterend",
+        `<video controls style="width: 100%;height: auto;" src="${
+          videoLink.href
+        }"></video>`
+      );
+
+    videoLink.hasAlreadyBeenUpdated = true;
+  }
+}, 3000);
+})();
